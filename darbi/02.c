cc33ef0574e8257146524d90b4070045d12be5fa
user <rilevs17@gmail.com>
2019-09-23 17:59:08 +0300

    history_20190923_17_54

---
+++ darbi/02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+printf("Hello World!\n");
+printf("Hello World!\r");
+printf("Hello World!\t");
+printf("Hello World!\a");
+printf("Hello World!\'");
+printf("Hello World!\"");
+printf("Hello World!\?");
+printf("Hello World!\\/");
+printf("Hello World!\n");
+}
